57a03807a503c813cdaedfe6c05ac04e2b912534
hpazk <hpazk.zkh@gmail.com>
2021-02-07 14:59:47 +0700

    basic echo

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type M map[string]interface{}
 
 func main() {
 	fmt.Println("running...")
+
+	e := echo.New()
+
+	e.GET("/users", func(c echo.Context) error {
+		response := M{"message": "success"}
+
+		return c.JSON(http.StatusOK, response)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
